56d684c303b36ef074197bd2571ee0c7970a21d5
1130838 <1130838@isep.ipp.pt>
2015-05-04 22:43:13 +0100

    Initial commit

---
+++ src/tcp_chat_cli.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+class tcp_chat_cli {
+    static InetAddress IPdestino;
+    static Socket sock;
+
+
+    public static void main(String args[]) throws Exception {
+
+        int test2=10;
+        String nick, frase;
+        byte[] data = new byte[300];
+
+        if (args.length != 1) {
+            System.out.println("Deve indicar o servidor na linha de comando");
+            System.exit(1);
+        }
+
+        try {
+            IPdestino = InetAddress.getByName(args[0]);
+
+        } catch (UnknownHostException ex) {
+            System.out.println("O nome do servidor fornecido (" + args[0] + ") nao e valido");
+            System.exit(1);
+        }
+
+        try {
+            sock = new Socket(IPdestino, 9999);
+        } catch (IOException ex) {
+            System.out.println("A ligacao TCP com o servidor falhou.");
+            System.exit(1);
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream sOut = new DataOutputStream(sock.getOutputStream());
+
+        System.out.print("Nickname: ");
+        nick = in.readLine();
+
+        Thread serverConn = new Thread(new tcp_chat_cli_con(sock, test2));
+        serverConn.start();
+
+
+        Thread serverConn2 = new Thread(new tcp_chat_cli_con(sock, test2));
+        serverConn2.start();
+
+        while (true) {
+            frase = in.readLine();
+            if (frase.compareTo("sair") == 0) {
+                sOut.write(0);
+                break;
+            }
+            frase = "(" + nick + ") " + frase;
+            data = frase.getBytes();
+            sOut.write((byte) frase.length());
+            sOut.write(data, 0, (byte) frase.length());
+
+
+        }
+
+        serverConn.join();
+        sock.close();
+    }
+}
+
+class tcp_chat_cli_con implements Runnable {
+    private Socket s;
+    private int teste;
+    private DataInputStream sIn;
+
+
+    public tcp_chat_cli_con(Socket tcp_s, int test) {
+        s = tcp_s;
+        this.teste = test;
+    }
+
+    public void run() {
+        int nChars;
+        byte[] data = new byte[300];
+        String frase;
+
+        try {
+            sIn = new DataInputStream(s.getInputStream());
+            while (true) {
+
+                nChars = sIn.read();
+                if (nChars == 0) break;
+                sIn.read(data, 0, nChars);
+                frase = new String(data, 0, nChars);
+                System.out.println(frase);
+                teste++;
+                System.out.println(this.teste);
+
+            }
+        } catch (IOException ex) {
+            System.out.println("Ligacao TCP terminada.");
+        }
+    }
+}
+
---
+++ src/tcp_chat_srv.java
@@ -0,0 +1,79 @@
+import java.io.*; 
+import java.net.*; 
+import java.util.concurrent.*;
+
+class tcp_chat_srv
+{    
+public static int MAXCLI=100;
+public static Socket[] cliSock = new Socket[MAXCLI];
+public static DataOutputStream[] sOut = new DataOutputStream[MAXCLI];
+public static Boolean[] inUse= new Boolean[MAXCLI];
+public static Semaphore changeLock = new Semaphore(1);
+
+static ServerSocket sock;
+
+public static void main(String args[]) throws Exception    
+{       
+int i;
+
+try { sock = new ServerSocket(9999); }
+catch(IOException ex) {
+	System.out.println("O porto local esta ocupado.");
+	System.exit(1); }
+
+for(i=0; i<MAXCLI; i++) inUse[i]=false;
+while(true)
+	{
+	changeLock.acquire();
+	for(i=0; i<MAXCLI; i++) if(!inUse[i]) break; // procurar um socket livre
+	changeLock.release();
+	cliSock[i]=sock.accept(); // esperar por lig. de novo cliente
+	changeLock.acquire();
+	inUse[i]=true;
+	sOut[i]=new DataOutputStream(cliSock[i].getOutputStream());
+	changeLock.release();
+	new Thread(new tcp_chat_srv_conn(i)).start();
+	}
+} 
+}
+
+
+
+class tcp_chat_srv_conn implements Runnable 
+{    
+int myNum;
+private DataInputStream sIn;
+
+public tcp_chat_srv_conn(int cli_n) { myNum=cli_n;}
+
+public void run()
+{
+int i, nChars;
+byte[] data = new byte[300];
+
+try {
+sIn = new DataInputStream(tcp_chat_srv.cliSock[myNum].getInputStream());
+while(true)
+   {
+   nChars=sIn.read();
+   if(nChars==0) break; // linha vazia
+   sIn.read(data,0,nChars);
+   tcp_chat_srv.changeLock.acquire();
+   for(i=0; i<tcp_chat_srv.MAXCLI; i++) // retransmitir a linha
+	if(tcp_chat_srv.inUse[i])
+		{
+		tcp_chat_srv.sOut[i].write(nChars);
+		tcp_chat_srv.sOut[i].write(data,0,nChars);
+		}
+   tcp_chat_srv.changeLock.release();
+   }
+   // o cliente quer sair
+   tcp_chat_srv.changeLock.acquire();
+   tcp_chat_srv.sOut[myNum].write(nChars);
+   tcp_chat_srv.inUse[myNum]=false;
+   tcp_chat_srv.cliSock[myNum].close();
+   tcp_chat_srv.changeLock.release();
+} catch(IOException ex) { System.out.println("IOException"); }
+  catch(InterruptedException ex) { System.out.println("Interrupted"); }
+}
+}
